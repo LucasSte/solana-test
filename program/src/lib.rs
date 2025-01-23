66f977c451bfa1db4fdcda4f34321cfe3f11876a
Lucas <lucas.tnagel@gmail.com>
2025-01-23 19:47:59 -0300

    Test syscalls

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,56 +1,50 @@
-#![feature(panic_info_message)]
-
-use pinocchio_log::logger::{Argument, Logger};
-use solana_program::{
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-};
 
 
-entrypoint!(process_instruction);
 
-fn process_instruction(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
+#[no_mangle]
+pub unsafe extern "C" fn entrypoint(input: *mut u8) -> u64 {
+   let n1 = u64::from_le_bytes(std::slice::from_raw_parts(input, 8).try_into().unwrap());
+   let n2 = u64::from_le_bytes(std::slice::from_raw_parts(input.add(16), 8).try_into().unwrap());
+   let src = input.add(32);
+   let dst = input.add(n1 as usize);
 
-    // let a = instruction_data[85034];
-    // let a : Option<u64> = None;
-    // let b = a.unwrap();
-    Ok(())
+   // memcpy
+   // 7 instr to check + 10 to syscall + 5 after syscall = 22
+   // 7 instr to check + 22 = 29
+   //
+   // Best case 7 + 5 = 12
+   // Intermediate 7 + 5 + 9 = 21
+   // Worst case 29
+   //
+   // We need at least 11 loads/stores before calling = 22 instr
+   std::ptr::copy_nonoverlapping(src, dst, n2 as usize);
+
+   let src = input.add(64);
+   let dst = input.add(90);
+
+   // memmove
+   // 7 instr to check + 10 to syscall + 5 after syscall = 22
+   // Case 1: 8 + 22 = 30
+   // Case 2: 8 + 33 = 41
+   //
+   // We need at least 11 loads/stores before calling = 22 instr
+   std::ptr::copy(src, dst, n2 as usize);
+
+   let mut p = std::slice::from_raw_parts_mut(input.add(128), n1 as usize);
+   p.fill(0);
+   
+   let p1 = std::slice::from_raw_parts(input.add(256), n2 as usize);
+   let p2 = std::slice::from_raw_parts(input.add(512), n2 as usize);
+
+   if p1 == p2 {
+    1
+   } else {
+    0
+   }
 }
 
 #[no_mangle]
 fn custom_panic(info: &core::panic::PanicInfo<'_>) {
-    if let Some(loc) = info.location() {
-        const MAX_LEN : usize = 200;
-        let filename = loc.file();
-        // MAX_LEN for filename + 3 (ellipsis string) + 11 (line number + separator)
-        // + 11 (column number + separator) + 13 (panic string) + 15 extra bytes
-        let mut logger = Logger::<{MAX_LEN + 53}>::default();
 
-        logger.append("Panicket at: "); // 13 bytes
-        
-        logger.append_with_args(filename, &[Argument::Precision(MAX_LEN as u8)]);
-
-        logger.append(":");
-        logger.append(loc.line());
-        logger.append(":");
-        logger.append(loc.column());
-        
-        logger.log();
-    }
-
-    if let Some(Some(mm)) = info.message().map(|mes| mes.as_str()) {
-        let mes = mm.as_bytes();
-        // SAFETY: We assumed the panic message is well formed.
-        unsafe {
-            solana_program::syscalls::sol_log_(mes.as_ptr(), mes.len() as u64);
-        }
-    }
 }
-
 
